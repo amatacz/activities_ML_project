3115adaaa2effee96d63a3fe85027049c5442812
Aleksandra Matacz <aleksandramatacz@MacBook-Air-Aleksandra.local>
2025-10-27 20:41:48 +0100

    feature_engineering started

--- src/features.py
+++ src/features.py
@@ -45,5 +45,40 @@ def calc_avg_speed(time, distance):
     speed = (distance/time) * 3600
 
     return round(speed, 2)
+
+def calc_hr_zone(avg_hr, age=32):
+    """
+    Calculates HeartRate Zone for recorded activity.
+    """
+
+    hr_max = 220-age
+
+    if avg_hr/hr_max >= 0.90:
+        return 5
+    elif avg_hr/hr_max >= 0.80:
+        return 4
+    elif avg_hr/hr_max >= 0.70:
+        return 3
+    elif avg_hr/hr_max >= 0.60:
+        return 2
+    else:
+        return 1
+    
+def calc_calories_burning_intensity(kcal, time):
+    """
+    Calculates burned calories per minute.
+
+    Kcal -> int
+    time -> sec
+    """
+
+    if pd.isna(time) or time <= 0:
+        return 0.0
+    
+    if pd.isna(kcal) or kcal <= 0:
+        return 0.0
+    burning_intensity = kcal/(time/60) 
+    return round(burning_intensity, 2)
+
     
     
