63e61a1ded3fd01a53eb983f1f47a641cc67828a
Aleksandra Matacz <aleksandramatacz@MacBook-Air-Aleksandra.local>
2025-10-27 19:51:45 +0100

    first commit. Bronze data processed and exported to silver data layer

---
+++ helpers.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+
+def time_to_seconds(time_str):
+    """ Converts time from HH:MM:SS to seconds"""
+    if pd.isna(time_str) or time_str == '':
+        return np.nan
+    
+    parts = str(time_str).split(':')
+
+    if len(parts) == 3:
+        h, m, s = map(float, parts)
+    elif len(parts) == 2:
+        h = 0.0
+        m, s = map(float, parts)
+    else:
+        return 0.0
+    
+    return h * 3600 + m * 60 + s
+
+def pace_to_seconds_per_km(pace_str):
+    """ Converts pace from M:SS/km to seconds/km """
+
+    if pd.isna(pace_str) or pace_str == '':
+        return np.nan
+    
+    parts = str(pace_str).split(":")
+
+    if len(parts) == 2:
+        m, s = map(float, parts)
+        return m * 60 + s
+    
+
+def calc_avg_speed(time, distance):
+    """ Calculates average speed based on known values:
+     time -> in seconds
+     distance -> in kms
+      """
+    if pd.isna(time) or time <= 0:
+        return np.nan
+    
+    if pd.isna(distance) or distance == 0.0:
+        return np.nan
+    
+    speed = (distance/time) * 3600
+
+    return round(speed, 2)
+    
+    
